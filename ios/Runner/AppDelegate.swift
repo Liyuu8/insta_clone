944804d383b89a35f2bd6c63773232c289402562
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-08-08 19:19:00 +0900

    Google map setting for android and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // TODO: 環境変数の設定
+    NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"INSTA_CLONE_IOS_MAPS_API_KEY"];
+    GMSServices.provideAPIKey(mapsApiKey)
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
